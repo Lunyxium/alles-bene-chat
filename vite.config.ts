14462bd11500ea986027b48acebd8c2adae15bbb
Lunyxium <mathias@baeumli.dev>
2025-09-13 14:27:48 +0200

    MASTER UPDATE Updated dependencies, deleted postcss.config.js and tailwind.config.js completely because they no longer required with tailwindcss/vite 4.1.13.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: { '@': resolve(__dirname, 'src') }
-    }
+    plugins: [react(), tailwindcss()]
 })
