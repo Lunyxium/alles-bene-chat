aa7c4c85f76dcbcf5459d239e7edc168c246a57a
Lunyxium <mathias@baeumli.dev>
2025-09-27 15:36:03 +0200

    ---try rerouting to login---

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,11 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, Navigate } from 'react-router-dom'
 import { LoginPage, ChatPage, ProfilePage } from './pages'
 import { RequireAuth } from './auth'
 
 export const appRouter = createBrowserRouter([
-    { path: '/', element: <RequireAuth><ChatPage/></RequireAuth> },
-    { path: '/login', element: <LoginPage/> },
+    { path: '/', element: <Navigate to="/login" replace /> },
+    { path: '/login', element: <LoginPage /> },
+    { path: '/chat', element: <RequireAuth><ChatPage/></RequireAuth> },
     { path: '/profile', element: <RequireAuth><ProfilePage/></RequireAuth> },
-    { path: '*', element: <div className="p-6">Oh no… 404 🫠</div> }
+    { path: '*', element: <Navigate to="/login" replace /> }
 ])
